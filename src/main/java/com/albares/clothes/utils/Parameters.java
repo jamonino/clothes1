9fcd1d67afc909b70b96900c0759cdc8fa64e2d5
josem <josem@LAPTOP-S1P10O6I>
2022-02-23 13:22:57 +0100

    initsdff

--- src/main/java/com/albares/clothes/utils/Parameters.java
+++ src/main/java/com/albares/clothes/utils/Parameters.java
@@ -10,7 +10,6 @@ public final class Parameters {
     
     public static final String PROJECT_NAME = "Clothes";
         
-    
     public static final String DB_URL = "jdbc:postgresql://localhost:5432/clothes_db";
     public static final String DB_USER = "clothes_user";
     public static final String DB_PASS = Secrets.DB_PASS;
